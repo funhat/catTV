5b6f6ec8b873ee469910d9112073b02c247b84f1
waihoyu <13701343809@qq.com>
2018-06-05 00:24:21 +0800

    '代码'

---
+++ utils/util.js
@@ -0,0 +1,54 @@
+const formatTime = date => {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+const $get = (url, data) => {
+    const p = new Promise((resolve, reject) => {
+        wx.request({
+                url,
+                data,
+                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                header: { 'Content-Type': 'json' }, // 设置请求的 header
+                success: resolve,
+                fail: reject,
+                complete: function() {
+                    // complete
+                }
+            })
+            //缺点
+            //回调地狱  面试必问的面试题
+            // wx.request({
+            //     url,
+            //     success: (data) => {
+            //         wx.request({
+            //             url2,
+            //             success: (data) => {
+            //                 wx.request({
+            //                     url3,
+            //                     success: (data) => {
+
+        //                     }
+        //                 })
+        //             }
+        //         })
+        //     }
+        // })
+    });
+    return p;
+}
+module.exports = {
+    formatTime: formatTime,
+    $get
+}
